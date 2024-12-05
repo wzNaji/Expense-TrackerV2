5880cca47c55c55c8d03e4191b5a42bda99782f2
wzNaji <ludi2300@gmail.com>
2024-12-05 19:06:40 +0100

    minor

---
+++ src/test/java/com/wzn/expensetrackerv2/ExpenseTrackerV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wzn.expensetrackerv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseTrackerV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
