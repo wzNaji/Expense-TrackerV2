3a817fc3a06ed9f4d1e1d2d3bb2823f9b19326f0
wzNaji <walid.z.naji@gmail.com>
2024-12-09 10:40:32 +0100

    Minor changes. Removed unused classes and Changed 'localhost' to 127.0.0...

--- src/main/java/com/wzn/expensetrackerv2/constants/SecurityConstants.java
+++
@@ -1,8 +0,0 @@
-package com.wzn.expensetrackerv2.constants;
-
-public class SecurityConstants {
-
-    public static final String JWT_KEY = System.getenv("JWT_KEY");
-    public static final String JWT_HEADER = "Authorization";
-
-}
